62b7e02d4b1131790c7380d09da93e0708d05fb5
Steler <stelerbw@seznam.cz>
2026-09-10 17:15:15 +0200

    fix

--- Tests.cs
+++ Tests.cs
@@ -5,14 +5,21 @@ using TestProject3.Pages;
 namespace TestProject3
 {
     [TestClass]
-    public class Seznam : PageTest
+    public class Seznam
     {
         private HomePageSeznam homePage = null!;
 
         [TestInitialize]
         public async Task Setup()
         {
-            homePage = new HomePageSeznam(Page);
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true,
+                //SlowMo = 100
+            });
+
+            homePage = new HomePageSeznam(await browser.NewPageAsync());
             await homePage.GotoAsync();
             await homePage.ConsentAsync();
         }
@@ -26,33 +33,33 @@ namespace TestProject3
         [TestMethod]
         public async Task UrlSeznam()
         {
-            await Expect(homePage._page).ToHaveURLAsync(new Regex("https://www.seznam.cz"));
+            await Assertions.Expect(homePage._page).ToHaveURLAsync(new Regex("https://www.seznam.cz"));
         }
 
         [TestMethod]
         public async Task LogoSeznam()
         {
-            await Expect(homePage.LogoLocator).ToHaveCountAsync(4);
+            await Assertions.Expect(homePage.LogoLocator).ToHaveCountAsync(4);
         }
 
         [TestMethod]
         public async Task MainpageSeznam()
         {
-            await Expect(homePage.MainContentLocator).ToBeVisibleAsync();
+            await Assertions.Expect(homePage.MainContentLocator).ToBeVisibleAsync();
         }
 
         [TestMethod]
         public async Task ServicesSeznam()
         {
-            await Expect(homePage.ServicesLocator).ToHaveCountAsync(23);
+            await Assertions.Expect(homePage.ServicesLocator).ToHaveCountAsync(23);
         }
 
         [TestMethod]
         public async Task WeatherNowAndTodaySeznam()
         {
-            await Expect(homePage.WeatherLocator.Last).ToHaveTextAsync(new Regex(@"\d"));
+            await Assertions.Expect(homePage.WeatherLocator.Last).ToHaveTextAsync(new Regex(@"\d"));
 
-            await Expect(homePage.WeatherLocator.Nth(3)).ToHaveTextAsync(new Regex(@"\d"));
+            await Assertions.Expect(homePage.WeatherLocator.Nth(3)).ToHaveTextAsync(new Regex(@"\d"));
         }
 
         [TestMethod]
@@ -60,9 +67,9 @@ namespace TestProject3
         {
             var searachPage = await homePage.SearchAsync("Playwright");
 
-            await Expect(searachPage._page).ToHaveURLAsync(new Regex("https://search.seznam.cz/"));
+            await Assertions.Expect(searachPage._page).ToHaveURLAsync(new Regex("https://search.seznam.cz/"));
 
-            await Expect(searachPage.ResultsLocator.First).ToHaveTextAsync(new Regex(@"playwright.dev"));
+            await Assertions.Expect(searachPage.ResultsLocator.First).ToHaveTextAsync(new Regex(@"playwright.dev"));
         }
     }
 }
